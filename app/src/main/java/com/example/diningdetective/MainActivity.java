077036bfe45c9b5efdeee5063975adebb4a90f91
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2021-10-23 16:56:39 -0400

    Added logic to take in input data and time into table

--- app/src/main/java/com/example/diningdetective/MainActivity.java
+++ app/src/main/java/com/example/diningdetective/MainActivity.java
@@ -10,26 +10,109 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    //not busy -> 0; neutral busy -> 1; very busy -> 2
+    int busyNum;
+    //low food -> 0; medium food -> 1; high food -> 2
+    int foodNum;
+    //brittain -> 0; west -> 1; nav -> 2
+    int diningHallNum = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        makeFile();
+        String[] columnTitles = new String[] {"Day","Time","Busy Level", "Food Availability"};
+        ArrayList<String[]> brittainData = new ArrayList<>();
+        brittainData.add(0, columnTitles);
+        ArrayList<String[]> westData = new ArrayList<>();
+        westData.add(0, columnTitles);
+        ArrayList<String[]> navData = new ArrayList<>();
+        navData.add(0, columnTitles);
+        RadioGroup busyGroup = findViewById(R.id.busyGroup);
+        RadioGroup foodGroup = findViewById(R.id.foodGroup);
+        busyGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.notBusy) {
+                busyNum = 0;
+            } else  if (checkedId == R.id.neutralBusy) {
+                busyNum = 1;
+            } else  if (checkedId == R.id.veryBusy) {
+                busyNum = 2;
+            }
+        });
+        foodGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.lowFood) {
+                foodNum = 0;
+            } else  if (checkedId == R.id.mediumFood) {
+                foodNum = 1;
+            } else  if (checkedId == R.id.highFood) {
+                foodNum = 2;
+            }
+        });
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.dining_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if(position == 0) {
+                    diningHallNum = 0;
+                }
+                else if(position == 1) {
+                    diningHallNum = 1;
+                }
+                else {
+                    diningHallNum = 2;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+
+        });
+        Button submitButton = findViewById(R.id.submission);
+        submitButton.setOnClickListener(v -> {
+            if(busyGroup.getCheckedRadioButtonId() != -1 && foodGroup.getCheckedRadioButtonId() != -1) {
+                String[] data = new String[4];
+                data[0] = "" + (Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 1);
+                data[1] = "" + Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+                data[2] = "" + busyNum;
+                data[3] = "" + foodNum;
+                if(diningHallNum == 0) {
+                    brittainData.add(data);
+                } else if(diningHallNum == 1) {
+                    westData.add(data);
+                } else {
+                    navData.add(data);
+                }
+                Toast.makeText(getBaseContext(), "Sucessfully submitted", Toast.LENGTH_SHORT).show();
+            } else if(busyGroup.getCheckedRadioButtonId() == -1) {
+                Toast.makeText(getBaseContext(), "Select how busy the dining hall is", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getBaseContext(), "Select how much food is in the dining hall", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void makeFile() {
         Context context = getApplicationContext();
         String data="my Info to save";
 
         try(FileInputStream fin = context.openFileInput("text.txt")) {
+            fin.close();
             Toast.makeText(getBaseContext(), "File exists", Toast.LENGTH_SHORT).show();
         } catch (FileNotFoundException e) {
             File file = new File(context.getFilesDir(), "text.txt");
@@ -59,21 +142,5 @@ public class MainActivity extends AppCompatActivity {
         catch(Exception e){
             Toast.makeText(getBaseContext(), "error", Toast.LENGTH_SHORT).show();
         }
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.dining_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-            }
-
-        });
     }
 }
